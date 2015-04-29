cd32d500e15f607a089a5d5e101b4f63962fe82a
Franton Lin <franton.lin@students.olin.edu>
2015-04-29 10:37:31 -0400

    plotting in k-space not working

--- fft.py
+++ fft.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 import scipy.fftpack
 from scipy.io import wavfile
 from scipy.signal import find_peaks_cwt
-from utility import pcm2float
 import scikits.audiolab
 from numpy.random import normal
 
@@ -19,6 +18,8 @@ def plotSpectrum(y, Fs):
 
     Y = scipy.fftpack.fft(y) # fft computing
     Y = Y[range(n/2)] # one side
+    print len(Y)
+    Y = Y[:900]
 
     Ts = 1.0/Fs; # sampling interval
     t = np.arange(0,n*Ts,Ts) # time vector
@@ -28,33 +29,51 @@ def plotSpectrum(y, Fs):
     plt.xlabel('Time')
     plt.ylabel('Amplitude')
 
-    plt.subplot(2,2,3)
+    plt.subplot(2,2,2)
     plt.hold(True)
     absY = abs(Y)
     for i in range(len(absY)):
         plt.plot([frq[i],frq[i]],(0,absY[i]),'r',markersize=0.1)
     plt.xlabel('Freq (Hz)')
     plt.ylabel('|Y(freq)|')
-    plt.xlim([0, 5000])
+    #plt.xlim([0, 5000])
 
-    peak_locs = find_peaks_cwt(absY,np.arange(2,3))
+    peak_locs = find_peaks_cwt(absY,np.arange(1,3),noise_perc=10)
     print peak_locs
 
     plt.subplot(2,2,4)
     peak_frq = [frq[i] for i in peak_locs]
     peak_Y = [absY[i] for i in peak_locs]
-    plt.plot(peak_frq, peak_Y, 'g')
-    plt.xlim([0, 5000])
+    plt.plot(peak_frq, peak_Y, 'gx-')
+    #plt.xlim([0, 5000])
+
+    #k_locs = find_peaks_cwt(peak_Y,np.arange(2,3))#,noise_perc=8)
+    k_locs = get_local_maxes(peak_Y)
+    plt.subplot(2,2,3)
+    k_frq = [peak_frq[i] for i in k_locs]
+    k_Y = [peak_Y[i] for i in k_locs]
+    print k_Y
+    plt.plot(k_frq, k_Y, '*')
+    #plt.xlim([0, 5000])
 
     plt.show()
 
-Fs, sig = wavfile.read("ee.wav")
+def get_local_maxes(a):
+    locs = []
+    for i in range(len(a)):
+        if i > 0 and i < len(a)-1:
+            if a[i] > a[i-1] and a[i] > a[i+1] and a[i] > 20:
+                locs.append(i)
+    return locs
+
+Fs, sig = wavfile.read("men/m01ae.wav")
+print Fs
 
 n = len(sig)
 Ts = 1.0/Fs; # sampling interval
 
 # cut to only vowel
-sig = sig[int(n*0.11/(n*Ts)):int(n*0.3/(n*Ts))]
+sig = sig[int(n*0.4):int(n*0.45)]
 
 
 #norm = pcm2float(sig, 'float32')
