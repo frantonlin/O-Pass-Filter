787ce9f6c16eae61d09fe9bf343fb2aedcbf0568
Franton Lin <franton.lin@students.olin.edu>
2015-05-04 20:44:53 -0400

    alsa audio period is confusing

--- fft.py
+++ fft.py
@@ -2,13 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.fftpack
 from scipy.io import wavfile
-from scipy.signal import argrelmax
+from scipy.signal import argrelmax, argrelmin
 import scikits.audiolab
 
-def findFormants(y, Fs, plot=True):
+def findFormants(y, Fs, plot=False):
     # number of samplepoints
     n = len(y)
-    print n
 
     # sample spacing
     T = n/float(Fs)
@@ -16,9 +15,9 @@ def findFormants(y, Fs, plot=True):
     frq = k/T # two sides frequency range
     frq = frq[range(n/2)] # one side frequency range
 
-    Y = scipy.fftpack.fft(y) # fft computing
+    Y = scipy.fftpack.rfft(y) # fft computing
     Y = Y[range(n/2)] # one side
-    Y = Y[:900]
+    Y = Y[:0.3*n]
 
     Ts = 1.0/Fs; # sampling interval
     t = np.arange(0,n*Ts,Ts) # time vector
@@ -26,13 +25,18 @@ def findFormants(y, Fs, plot=True):
     absY = abs(Y)
 
     peak_locs = argrelmax(absY, order=2)[0]
+    trough_locs = argrelmin(absY, order=4)[0]
+    extrema_locs = np.sort(np.concatenate((peak_locs, trough_locs)))
 
-    peak_frq = [frq[i] for i in peak_locs]
-    peak_Y = [absY[i] for i in peak_locs]
+    #peak_frq = [frq[i] for i in peak_locs]
+    #peak_Y = [absY[i] for i in peak_locs]
 
-    k_locs = argrelmax(np.array(peak_Y))[0]
-    k_frq = [peak_frq[i] for i in k_locs]
-    k_Y = [peak_Y[i] for i in k_locs]
+    extrema_frq = [frq[i] for i in extrema_locs]
+    extrema_Y = [absY[i] for i in extrema_locs]
+
+    k_locs = argrelmax(np.array(extrema_Y))[0]
+    k_frq = [extrema_frq[i] for i in k_locs]
+    k_Y = [extrema_Y[i] for i in k_locs]
 
     if plot:
         plt.subplot(2,2,1)
@@ -48,7 +52,7 @@ def findFormants(y, Fs, plot=True):
         plt.ylabel('|Y(freq)|')
 
         plt.subplot(2,2,4)
-        plt.plot(peak_frq, peak_Y, 'gx-')
+        plt.plot(extrema_frq, extrema_Y, 'gx-')
         plt.plot(k_frq, k_Y, '*')
 
         plt.subplot(2,2,3)
@@ -60,11 +64,31 @@ def findFormants(y, Fs, plot=True):
 
     return k_frq[:3]
 
-    def parseAudio(data):
-        pass
+def parseAudio(data, Fs, plot=False):
+    formants = []
+    """for i in range(len(data)/320):
+        frame = data[i*320:i*320+320]
+        window = np.hanning(len(frame))
+        hanned = frame*window
+        formants.append(findFormants(hanned, Fs))
+
+    if plot:
+        for frame in formants:
+            plt.plot(frame[1],frame[0],'rx')
+        plt.show()
+    return formants"""
+    i = 40
+    print len(data)
+    framelen = 1000
+    frame = data[i*framelen:i*framelen+framelen]
+    window = np.hanning(len(frame))
+    hanned = frame*window
+    #scikits.audiolab.play(data, fs=Fs)
+    formants.append(findFormants(hanned, Fs, True))
+    return formants
 
 
-Fs, sig = wavfile.read("men/m01ae.wav")
+"""Fs, sig = wavfile.read("men/m01ae.wav")
 print Fs
 
 n = len(sig)
@@ -84,4 +108,13 @@ print hanned
 
 scikits.audiolab.play(hanned, fs=Fs)
 
-print findFormants(hanned, Fs, plot=True)
+print findFormants(hanned, Fs, plot=True)"""
+
+
+"""Fs, sig = wavfile.read("men/m01ae.wav")
+norm = sig/float(max(sig)*2)
+scikits.audiolab.play(norm, fs=Fs)
+formants = parseAudio(norm, Fs)"""
+#for frame in formants:
+#    plt.plot(frame[1],frame[0],'rx')
+#plt.show()
--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import alsaaudio
+import pyaudio
 import pyglet
 from pyglet.window import key
 import scikits.audiolab
 import numpy as np
 import struct
+from fft import parseAudio
+
+SF = 44100
+PERIOD = 470
 
 class Filter(pyglet.window.Window):
     def __init__(self):
         pyglet.window.Window.__init__(self, width=512, height=512,visible=True)
         self.recording = False
-        self.inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE,0)
-        self.inp.setformat(alsaaudio.PCM_FORMAT_FLOAT_LE)
-        self.inp.setchannels(1)
-        self.inp.setrate(16000) # Sets sampling rate to 16000 Hz
-        self.inp.setperiodsize(320)
         self.data = []
 
     def on_draw(self):
@@ -23,15 +23,21 @@ class Filter(pyglet.window.Window):
     def on_key_press(self, symbol, modifiers):
         if symbol == key.SPACE:
             self.recording = True
+            self.inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE,0)
+            self.inp.setformat(alsaaudio.PCM_FORMAT_FLOAT_LE)
+            self.inp.setchannels(1)
+            self.inp.setrate(SF) # Sets sampling rate to SF Hz
+            self.inp.setperiodsize(PERIOD)
 
     def on_key_release(self, symbol, modifiers):
         if symbol == key.SPACE:
             self.recording = False
+            self.inp.pause(1)
             #perform filtering
             self.data = np.array(self.data)
             norm = self.data/max(self.data)
-            scikits.audiolab.play(norm, fs=16000)
-            #print norm
+            scikits.audiolab.play(norm, fs=SF)
+            parseAudio(norm, SF, True)
             pyglet.app.exit()
 
 
@@ -39,11 +45,11 @@ class Filter(pyglet.window.Window):
         if self.recording:
             l, data = self.inp.read()
             if data:
-                floats = struct.unpack('f'*320,data)
+                floats = struct.unpack('f'*PERIOD,data)
                 self.data.extend(floats)
 
     def run(self):
-        pyglet.clock.schedule_interval(self.update, 1/16000.0)
+        pyglet.clock.schedule_interval(self.update, 1.0/SF)
         pyglet.app.run()
 
 if __name__ == '__main__':
